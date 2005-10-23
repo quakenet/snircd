c97f52230d4b5b82a1142032cbb3da6793285b49
paul <devnull@localhost>
2005-10-23 23:41:00 +0100

    forward port of asuka-lamekillfix.patch to .12

--- ircd/IPcheck.c
+++ ircd/IPcheck.c
@@ -285,9 +285,7 @@ int ip_registry_check_local(const struct irc_in_addr *addr, time_t* next_target_
    */
   if (0 == ++entry->connected)
   {
-    entry->connected--;
-    Debug((DEBUG_DNS, "IPcheck refusing local connection from %s: counter overflow.", ircd_ntoa(&entry->addr)));
-    return 0;
+    ++entry->connected;
   }
 
   if (CONNECTED_SINCE(entry->last_connect) > IPCHECK_CLONE_PERIOD)
